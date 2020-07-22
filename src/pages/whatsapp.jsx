30f7c41e5676d0267cdc936b05e7e366602847e8
kleber <alejandrofaesta@gmail.com>
2020-07-22 09:44:19 -0400

    new cell

--- src/pages/whatsapp.jsx
+++ src/pages/whatsapp.jsx
@@ -13,7 +13,7 @@ const Whatsapp = ({ location }) => {
     });
     setTimeout(() => {
       if (typeof window !== "undefined") {
-        window.location.href = `https://api.whatsapp.com/send?phone=59177778354&text=${messageWhatsapp}`;
+        window.location.href = `https://api.whatsapp.com/send?phone=59174188223&text=${messageWhatsapp}`;
       }
     }, 300);
   }, []);
