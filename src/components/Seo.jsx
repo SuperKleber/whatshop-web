0af0c0d7d52f70f72188dc272a318fe757d8e1ea
kleber <alejandrofaesta@gmail.com>
2020-08-08 19:59:45 -0400

    entrega

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -7,27 +7,36 @@ import {
   siteLanguage,
   siteImage,
 } from "../../config/seo.json";
-const Seo = ({ title }) => {
+const Seo = ({ title, description }) => {
   return (
     <Helmet>
       <title>{title ? title : siteTitle}</title>
       <meta http-equiv="content-language" content={siteLanguage}></meta>
       <meta name="language" content="Spanish"></meta>
-      <meta name="description" content={siteDescription} />
+      <meta
+        name="description"
+        content={description ? description : siteDescription}
+      />
       <meta name="image" content={siteImage} />
       <link rel="canonical" href={siteUrl} />
       {/* OpenGraph tags */}
       <meta property="og:url" content={siteUrl} />
-      <meta property="og:title" content={siteTitle} />
+      <meta property="og:title" content={title ? title : siteTitle} />
       <meta property="og:type" content={"website"} />
 
-      <meta property="og:description" content={siteDescription} />
+      <meta
+        property="og:description"
+        content={description ? description : siteDescription}
+      />
       <meta property="og:image" content={siteImage} />
 
       {/* Twitter Card tags */}
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content={siteTitle} />
-      <meta name="twitter:description" content={siteDescription} />
+      <meta name="twitter:title" content={title ? title : siteTitle} />
+      <meta
+        name="twitter:description"
+        content={description ? description : siteDescription}
+      />
       <meta name="twitter:image" content={siteImage} />
     </Helmet>
   );
